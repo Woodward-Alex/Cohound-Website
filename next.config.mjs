a338cc5c239b91bb77ae44cc4d6a9655341531f7
awoodward94 <Alexandrawoodward@hotmail.co.uk>
2025-10-08 11:47:56 +0100

    simplify next config and remove PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,11 @@
-import withPWA from "next-pwa"
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true, // Ignore ESLint errors during build
+    ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true, // Ignore TypeScript errors during build
+    ignoreBuildErrors: true,
   },
-  // ... rest of your config
 }
 
-export default withPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-})(nextConfig)
+export default nextConfig
